73425729815734edfc618a5186aaa1876f87eb71
andryc <charlier_andry@hotmail.com>
2014-05-15 20:54:51 +0200

    bootstrap + includes
    bootstrap css en custom css toevoegen.
    nav en head include

--- home.php
+++ home.php
@@ -3,70 +3,42 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Hospitality</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />	
-	<link rel="stylesheet" href="css/normalize.css">
-	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/style.css">
-
-	<link rel="shortcut icon" href="images/favicon.ico">
-
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,800' rel='stylesheet' type='text/css'>
-	<link href='http://fonts.googleapis.com/css?family=Droid+Serif:400italic' rel='stylesheet' type='text/css'>
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700' rel='stylesheet' type='text/css'>
-
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	
+  <?php include_once('includes/incl.head.php'); ?>
 </head>
 <body>
-	<nav class="navbar navbar-default" role="navigation">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">Brand</a>
+<?php include_once('includes/incl.nav.php'); ?>
+<div class="row">
+  <div id="cta">
+    <div class="container">
+      <section id="cta-left" class="col-md-6">
+        <img src="http://dummyimage.com/400x200/000/fff" alt="hospitality responsive">
+      </section>
+
+      <section id="cta-left" class="col-md-6">
+        <p>
+          Hospitality
+        </p>
+      </section>
     </div>
+  </div>
+</div>
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Link</a></li>
-        <li><a href="#">Link</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-            <li class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
-          </ul>
-        </li>
-      </ul>
-      
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Link</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
+<div class="row">
+  <div id="">
+    <div class="container">
+      <section class="col-md-4 function-wrap">
+        <img src="http://dummyimage.com/300x200/000/fff" alt="hospitality responsive">
+      </section>
 
+      <section class="col-md-4 function-wrap">
+        <img src="http://dummyimage.com/300x200/000/fff" alt="hospitality responsive">
+      </section>
+
+      <section class="col-md-4 function-wrap">
+        <img src="http://dummyimage.com/300x200/000/fff" alt="hospitality responsive">
+      </section>
+    </div>
+  </div>
+</div>
 </body>
 </html>
